3fd2c47c8fc0039ba94f7458161805693e61e3f7
Seryeong <99seryoung50@gmail.com>
2022-04-14 17:15:35 +0900

    feat: 프로필 토글 추가

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Main from "./Pages/Main";
-import Login from "./Pages/Login";
-import SignUp from "./Pages/SignUp";
-import GIFBoard from "./Pages/GIFBoard";
+// Start
+import Login from "./Pages/Start/Login";
+import SignUp from "./Pages/Start/SignUp";
+// Board
+import GIFBoard from "./Pages/Board/GIFBoard";
+// Member
+import MyPage from "./Pages/Member/MyPage";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
-      <Route path="/login" element={<Login />} />
+      {/* Start */}
       <Route path="/signup" element={<SignUp />} />
+      <Route path="/login" element={<Login />} />
+      {/* Board */}
       <Route path="/boards" element={<GIFBoard />} />
+      {/* Member */}
+      <Route path="/mypage" element={<MyPage />} />
     </Routes>
   );
 };
