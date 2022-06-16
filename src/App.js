a9c43e1981aacf15c68dc5a08be563a43c091c7e
hyowon <hyowon9964@gmail.com>
2022-06-16 16:13:17 +0900

    Remove: 회원가입 삭제

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {Routes, Route} from "react-router-dom";
 import Main from "./Pages/Main";
 import Login from "./Pages/Start/Login";
-import SignUp from "./Pages/Start/SignUp";
 import Community from "./Pages/Board/Community";
 import LoginRedirect from "./Pages/Start/LoginRedirect";
 
@@ -11,7 +10,6 @@ const App = () => {
     <Routes>
       <Route exact path="/" element={<Main/>} />
       <Route path="/login" element={<Login/>} />
-      <Route path="/signup" element={<SignUp/>} />
       <Route path="/community" element={<Community/>} />
       <Route path="/oauth/kakao" element={<LoginRedirect/>} />
     </Routes>
