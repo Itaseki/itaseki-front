fb2678b1dff73c6b457a0b03eda6e6da1cb5a68f
hyowon <hyowon9964@gmail.com>
2022-07-06 11:32:46 +0900

    Design: 달리기 방 전체 레이아웃

--- src/Pages/Run/Running.js
+++ src/Pages/Run/Running.js
@@ -1,7 +1,67 @@
 import React from 'react';
+import {useNavigate} from "react-router-dom";
+import Header from "../../Components/Header";
+import {
+  Body,
+  ChatBody,
+  ChatBottom,
+  ChatInput,
+  Chatting,
+  ExitBtn, ImgBtn,
+  Inform, Information,
+  TopBar,
+  Video,
+  Wrapper
+} from "../../Style/Running";
+import Exit from "../../Assets/ExitRun.png";
+import ChatInformIcon from "../../Assets/Chat_Inform_icon.png";
+import ChatInform from "../../Assets/Chat_Inform.png";
+import ChatInformArrow from "../../Assets/Chat_Inform_Arrow.png";
+import SendingBtn from "../../Assets/Chat_Seding_btn.png";
+import ProfileImg from "../../Assets/Basic_profile.png";
 
 const Running = () => {
-  return <div>달리기</div>
+  const navigate = useNavigate();
+
+  // 달리기방 나가기
+  const onExit = () => {
+    const exit = window.confirm("달리기를 중단하시겠습니까?");
+    if(exit) navigate("/");
+  };
+
+  // 공지사항 내용 펼치기
+  const onShowInform = () => {
+
+  }
+
+  return (
+      <div style={{backgroundColor: "#F4F3EE"}}>
+        <Header/>
+        <Wrapper>
+          <ExitBtn src={Exit} alt="달리기 나가기" onClick={onExit}/>
+          <Body>
+            <Video>{"영상"}</Video>
+            <Chatting>
+              <TopBar />
+              <Inform>
+                <img src={ChatInformIcon} alt="공지사항 아이콘"/>
+                <img src={ChatInform} alt="공지사항 확인하기"/>
+                <img src={ChatInformArrow} alt="공지사항 화살표"
+                     style={{cursor: "pointer"}}
+                     onClick={onShowInform}/>
+                {/*<Information>공지사항 모달</Information>*/}
+              </Inform>
+              <ChatBody>{"채팅 내용, 임시 border"}</ChatBody>
+              <ChatBottom>
+                <ImgBtn src={ProfileImg} alt="프로필 이미지"/>
+                <ChatInput type="text"/>
+                <ImgBtn src={SendingBtn} alt="전송 버튼"/>
+              </ChatBottom>
+            </Chatting>
+          </Body>
+        </Wrapper>
+      </div>
+  )
 }
 
 export default Running;
---
+++ src/Style/Running.js
@@ -0,0 +1,78 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  margin: 20px;
+`
+
+export const ExitBtn = styled.img`
+  display: block;
+  margin-left: auto;
+  margin-right: 10px;
+  cursor: pointer;
+`
+
+export const Body = styled.div`
+  display: flex;
+`
+
+export const Video = styled.div`
+  width: 70%;
+  height: 0;
+  padding-bottom: 43.74%;
+  margin: 10px;
+  background-color: gray;
+`
+
+export const Chatting = styled.div`
+  width: 30%;
+  margin: 10px;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+`
+
+export const TopBar = styled.div`
+  background-color: #F9DCA7;
+  height: 22px;
+`
+
+export const Inform = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  margin: 5px;
+  padding: 3px;
+  background-color: #E8E8E8;
+`
+
+export const Information = styled.div`
+`
+
+export const ChatBody = styled.div`
+  height: 100%;
+  padding: 5px;
+  border: 1px solid;
+`
+
+export const ChatBottom = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 5px;
+  margin: 3px 0;
+`
+
+export const ImgBtn = styled.img`
+  width: 30px;
+  height: 30px;
+  margin: 0 5px;
+`
+
+export const ChatInput = styled.input`
+  background-color: #F9DCA7;
+  border: none;
+  border-radius: 71px;
+  width: 100%;
+  height: 0;
+  padding: 4%;
+  margin: 0 5px;
+`
