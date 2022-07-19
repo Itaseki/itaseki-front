bd2bf3a905d11d044eebbec1d536026a2b695825
Seryeong Kim <99seryoung50@gmail.com>
2022-07-20 02:10:01 +0900

    feat : 상단 Wrapper 추가

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import preURL from "../preURL/preURL";
@@ -7,78 +6,198 @@ import Header from "../Components/Header";
 import Footer from "../Components/Footer";
 
 // Assets
-import Home_running from "../Assets/Home_running.png";
 import Temp_gif from "../Assets/Temp_gif.png";
-=======
-import React from 'react';
-import {Link} from "react-router-dom";
->>>>>>> 2e0d404ad9ec1d07d87023bfee25f49cf244d0c5
+import Go_To_Run from "../Assets/Go_To_Run.png";
+import Main_Article from "../Assets/Main_Article.png";
+import Main_GIF from "../Assets/Main_GIF.png";
+import { StyledDiv, StyledDivColumn, StyledDivRow } from "../Style/StyledDiv";
+import StyledBtn from "../Style/StyledBtn";
+
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { light } from "../Style/Color";
+import { Link } from "react-router-dom";
 
 const Main = () => {
-  const [pVideos, setPVideos] = useState([
-    { image: Temp_gif, tags: "태그1" },
-    { image: Temp_gif, tags: "태그2" },
-    { image: Temp_gif, tags: "태그3" },
-    { image: Temp_gif, tags: "태그4" },
+  const [popCommus, setpopCommus] = useState([]);
+  const [popGIFs, setPopGIFs] = useState([
+    { id: 0, imageUrl: `${Temp_gif}`, likeCount: 100 },
+    { id: 0, imageUrl: `${Temp_gif}`, likeCount: 100 },
+    { id: 0, imageUrl: `${Temp_gif}`, likeCount: 100 },
+    { id: 0, imageUrl: `${Temp_gif}`, likeCount: 100 },
+    { id: 0, imageUrl: `${Temp_gif}`, likeCount: 100 },
   ]);
-  const [pPlaylists, setPPlaylists] = useState([
-    { image: Temp_gif, tags: "태그1" },
-    { image: Temp_gif, tags: "태그2" },
-    { image: Temp_gif, tags: "태그3" },
-    { image: Temp_gif, tags: "태그4" },
-  ]);
+  const [popVideos, setPopVideos] = useState([]);
+  const [popPlaylists, setPopPlaylists] = useState([]);
 
-  const PVideoList = pVideos.map((video, index) => (
-    <div key={index}>
-      <img
-        src={video.image}
-        style={{
-          width: "253px",
-          height: "142px",
-          marginTop: "17px",
-          marginBottom: "22px",
-        }}
-      />
-      <TagBox>
-        <Tag>#{video.tags}</Tag>
-      </TagBox>
-    </div>
-  ));
+  useEffect(() => {
+    console.log(
+      "=============================[Main.js]============================="
+    );
+    popCommu();
+    popGIF();
+    popVideo();
+    popPlaylist();
+  }, []);
 
-  const PPlaylistList = pPlaylists.map((playlist, index) => (
-    <div key={index}>
-      <img
-        src={playlist.image}
-        style={{
-          width: "253px",
-          height: "142px",
-          marginTop: "17px",
-          marginBottom: "22px",
-        }}
-      />
-      <TagBox>
-        <Tag>#{playlist.tags}</Tag>
-      </TagBox>
-    </div>
-  ));
+  // 인기 잡담글 조회
+  const popCommu = () => {
+    axios
+      .get(preURL.preURL + "/main/community")
+      .then((res) => {
+        console.log("❕인기 잡담글 조회❕ ", res.data);
+        setpopCommus(res.data);
+      })
+      .catch((err) => {
+        console.error("⚠️ 인기 잡담글 조회 ⚠️ ", err);
+      });
+  };
+
+  // 인기 짤 조회
+  const popGIF = () => {
+    axios
+      .get(preURL.preURL + "/main/image")
+      .then((res) => {
+        console.log("❕인기 짤 조회❕ ", res.data);
+        if (res.data.length > 1) {
+          setPopGIFs(res.data);
+        }
+      })
+      .catch((err) => {
+        console.error("⚠️ 인기 짤 조회 ⚠️ ", err);
+      });
+  };
+
+  // 인기 영상 조회
+  const popVideo = () => {
+    axios
+      .get(preURL.preURL + "/main/video")
+      .then((res) => {
+        console.log("❕인기 영상 조회❕ ", res.data);
+        setPopVideos(res.data);
+      })
+      .catch((err) => {
+        console.error("⚠️ 인기 영상 조회 ⚠️ ", err);
+      });
+  };
+
+  // 인기 플레이리스트 조회
+  const popPlaylist = () => {
+    axios
+      .get(preURL.preURL + "/main/playlist")
+      .then((res) => {
+        console.log("❕인기 플레이리스트 조회❕ ", res.data);
+        setPopPlaylists(res.data);
+      })
+      .catch((err) => {
+        console.error("⚠️ 인기 플레이리스트 조회 ⚠️ ", err);
+      });
+  };
 
   return (
-<<<<<<< HEAD
     <>
       <Header />
-      <FirstBWrapper>
-        <img
-          src={Home_running}
-          style={{ width: "593.02px", height: "321.81px" }}
-        />
-      </FirstBWrapper>
+      <StyledDiv>
+        <Link to="/running">
+          <img src={Go_To_Run} style={{ width: "624px", height: "415px" }} />
+        </Link>
+        <FirstBWrapper>
+          {/* 인기 게시글  */}
+          <PopArticleContainer>
+            <div style={{ paddingTop: 50, paddingLeft: 20 }}>
+              {popCommus.map((i) => {
+                let url = `/community/${i.id}`;
+                return (
+                  <Link to={url} style={{ textDecoration: "none" }}>
+                    <StyledBtn
+                      style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                        height: 26,
+                        width: 460,
+                      }}
+                    >
+                      <p style={{ fontSize: 13 }}>
+                        {i.title}
+                        {i.commentCount}
+                      </p>
+                      <StyledDivRow>
+                        <FontAwesomeIcon
+                          icon={faHeart}
+                          style={{
+                            marginRight: 5,
+                            color: `${light.colors.mainColor}`,
+                          }}
+                        />
+                        <p
+                          style={{
+                            color: `${light.colors.mainColor}`,
+                            fontSize: 12,
+                          }}
+                        >
+                          {i.likeCount}
+                        </p>
+                      </StyledDivRow>
+                    </StyledBtn>
+                  </Link>
+                );
+              })}
+            </div>
+          </PopArticleContainer>
+
+          {/* 인기 짤  */}
+          <PopGIFContainer>
+            <div style={{ paddingTop: 50, paddingLeft: 20 }}>
+              {popGIFs.map((i) => {
+                let url = `/boards/${i.id}`;
+                return (
+                  <Link to={url} style={{ textDecoration: "none" }}>
+                    <StyledBtn>
+                      <StyledDivColumn
+                        style={{
+                          margin: 5,
+                          marginLeft: 10,
+                          marginRight: 10,
+                          alignItems: "center",
+                        }}
+                      >
+                        <img
+                          src={i.imageUrl}
+                          style={{ width: 74, height: 67 }}
+                        />
+                        <StyledDivRow>
+                          <FontAwesomeIcon
+                            icon={faHeart}
+                            style={{
+                              marginRight: 5,
+                              color: `${light.colors.mainColor}`,
+                            }}
+                          />
+                          <p
+                            style={{
+                              color: `${light.colors.mainColor}`,
+                              fontSize: 12,
+                            }}
+                          >
+                            {i.likeCount}
+                          </p>
+                        </StyledDivRow>
+                      </StyledDivColumn>
+                    </StyledBtn>
+                  </Link>
+                );
+              })}
+            </div>
+          </PopGIFContainer>
+        </FirstBWrapper>
+      </StyledDiv>
       <PWrapper>
         <Subheading>지금 가장 인기 있는 영상</Subheading>
-        <ListWrapper>{PVideoList}</ListWrapper>
       </PWrapper>
       <PWrapper style={{ marginTop: "42px" }}>
         <Subheading>지금 가장 인기 있는 플레이리스트</Subheading>
-        <ListWrapper>{PPlaylistList}</ListWrapper>
       </PWrapper>
       <Footer />
     </>
@@ -87,8 +206,9 @@ const Main = () => {
 
 export default Main;
 
-const FirstBWrapper = styled.div`
-  margin: 22px 70px 74px 70px;
+const FirstBWrapper = styled(StyledDivColumn)`
+  margin-left: 15px;
+  margin-bottom: 15px;
 `;
 
 const TagBox = styled.div`
@@ -116,16 +236,26 @@ const Subheading = styled.p`
   font-weight: bold;
   margin: 0;
 `;
-=======
-      <div>
-        <h3>메인페이지</h3>
-        <Link to="/community">잡담게시판</Link>
-      </div>
-  )
-}
->>>>>>> 2e0d404ad9ec1d07d87023bfee25f49cf244d0c5
 
 const ListWrapper = styled.div`
   display: flex;
   justify-content: space-between;
+`;
+
+const PopArticleContainer = styled.div`
+  background-image: url(${Main_Article});
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+  width: 521px;
+  height: 169px;
+`;
+
+const PopGIFContainer = styled.div`
+  background-image: url(${Main_GIF});
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+  width: 521px;
+  height: 170px;
 `;
