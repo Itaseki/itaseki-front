0fa07de6f233a4ee2023ce1f6731935034b39feb
Seryeong Kim <99seryoung50@gmail.com>
2022-07-22 21:03:57 +0900

    feat/design : 짤 조회, 신고, 삭제 / 디자인 리뉴얼

--- src/Pages/Board/GIFBoard.js
+++ src/Pages/Board/GIFBoard.js
@@ -4,25 +4,26 @@ import preURL from "../../preURL/preURL";
 import styled from "styled-components";
 import Header from "../../Components/Header";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faHeart,
-  faCaretRight,
-  faCaretLeft,
-} from "@fortawesome/free-solid-svg-icons";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import { WithContext as ReactTags } from "react-tag-input";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import Pagination from "../../Components/Pagination";
 
 // style
 import StyledBtn from "../../Style/StyledBtn";
 import WriteBtn from "../../Style/WriteBtn";
 import WriteWord from "../../Style/WriteWord";
-import styles from "../../Style/GIFBoard.css";
+import { light } from "../../Style/Color";
+
 // assets
 import Best_GIF from "../../Assets/Best_GIF.png";
 import Temp_gif from "../../Assets/Temp_gif.png";
 import { Link } from "react-router-dom";
+import { StyledDiv, StyledDivColumn } from "../../Style/StyledDiv";
 
 const GIFBoard = () => {
+  const mainOrange = light.colors.mainColor;
+
   // 베스트 짤 리스트
   const [bestGIFs, setBestGIFs] = useState([
     { id: 1, imageUrl: Temp_gif, heart: 1 },
@@ -47,12 +48,12 @@ const GIFBoard = () => {
     { id: 1, imageUrl: Temp_gif, heart: 2 },
     { id: 1, imageUrl: Temp_gif, heart: 5 },
   ]);
-  // 페이지 목록, 가장 마지막 페이지일 때 ▷ 버튼을 클릭하면 페이지 번호+1
+  const [totalPage, setTotalPage] = useState(0);
+  const [page, setPage] = useState(0);
   const [pages, setPages] = useState([1, 2, 3, 4, 5]);
-  // 현재 페이지
-  const [page, setPage] = useState(1);
   // 정렬 기준
-  const [sort, setSort] = useState("id, DESC");
+  const [sort1, setSort1] = useState("likeCount,DESC");
+  const [sort2, setSort2] = useState("id,DESC");
 
   // 새 짤글 쓰기 ---------------------------------------------------
   // 새 짤글 쓰기 모달
@@ -88,98 +89,123 @@ const GIFBoard = () => {
       .get(preURL.preURL + "/boards/image/best")
       .then((res) => {
         console.log("❕베스트짤 조회❕ ", res.data);
+        setBestGIFs(res.data);
       })
       .catch((err) => {
         console.error("⚠️ 베스트짤 조회 ⚠️ ", err);
       });
   };
 
-  const bestGIFList = bestGIFs.map((gifs, index) => (
-    <div key={index} style={{ marginLeft: 12, marginRight: 12 }}>
-      {/* id 파라미터로 넘기기 수정 필요 */}
-      <Link to="/boards/:Id">
-        <StyledBtn>
-          <img src={gifs.imageUrl} style={{ width: 98, height: 86 }} />
-        </StyledBtn>
-      </Link>
-      <MapList>
-        <StyledBtn>
-          <FontAwesomeIcon
-            icon={faHeart}
-            style={{ fontSize: "80%", color: "#D9767C", marginRight: "4.32px" }}
-          />
-        </StyledBtn>
-        <p style={{ fontSize: "12px", color: "#D9767C" }}>{gifs.heart}</p>
-      </MapList>
-    </div>
-  ));
+  const bestGIFList = bestGIFs.map((gifs, index) => {
+    let url = `/boards/:${gifs.id}`;
+    return (
+      <div key={index} style={{ marginLeft: 12, marginRight: 12 }}>
+        <Link to={url}>
+          <StyledBtn>
+            <img
+              src={gifs.imageUrl}
+              alt="베스트짤 썸네일"
+              style={{ width: 98, height: 86 }}
+            />
+          </StyledBtn>
+        </Link>
+        <MapList>
+          <StyledBtn>
+            <FontAwesomeIcon
+              icon={faHeart}
+              style={{
+                fontSize: "80%",
+                color: "#D9767C",
+                marginRight: "4.32px",
+              }}
+            />
+          </StyledBtn>
+          <p style={{ fontSize: "12px", color: "#D9767C" }}>{gifs.heart}</p>
+        </MapList>
+      </div>
+    );
+  });
 
-  const gifList1 = gifs1.map((gifs, index) => (
-    <GIFBox key={index}>
-      {/* id 파라미터로 넘기기 수정 필요 */}
-      <Link to="/boards/:Id">
-        <StyledBtn>
-          <img src={gifs.imageUrl} style={{ width: 98, height: 86 }} />
-        </StyledBtn>
-      </Link>
-      <MapList>
-        <StyledBtn>
-          <FontAwesomeIcon
-            icon={faHeart}
-            style={{ fontSize: "80%", color: "#D9767C", marginRight: "4.32px" }}
-          />
-        </StyledBtn>
-        <p style={{ fontSize: "12px", color: "#D9767C" }}>{gifs.heart}</p>
-      </MapList>
-    </GIFBox>
-  ));
+  const gifList1 = gifs1.map((gifs, index) => {
+    let url = `/boards/:${gifs.id}`;
+    return (
+      <GIFBox key={index}>
+        <Link to={url}>
+          <StyledBtn>
+            <img
+              src={gifs.imageUrl}
+              alt="짤 썸네일"
+              style={{ width: 98, height: 86 }}
+            />
+          </StyledBtn>
+        </Link>
+        <MapList>
+          <StyledBtn>
+            <FontAwesomeIcon
+              icon={faHeart}
+              style={{
+                fontSize: "80%",
+                color: `${mainOrange}`,
+                marginRight: "4.32px",
+              }}
+            />
+          </StyledBtn>
+          <p style={{ fontSize: "12px", color: `${mainOrange}` }}>
+            {gifs.heart}
+          </p>
+        </MapList>
+      </GIFBox>
+    );
+  });
 
-  const gifList2 = gifs2.map((gifs, index) => (
-    <GIFBox key={index}>
-      {/* id 파라미터로 넘기기 수정 필요 */}
-      <Link to="/boards/:Id">
-        <StyledBtn>
-          <img src={gifs.imageUrl} style={{ width: 98, height: 86 }} />
-        </StyledBtn>
-      </Link>
-      <MapList>
-        <StyledBtn>
-          <FontAwesomeIcon
-            icon={faHeart}
-            style={{ fontSize: "80%", color: "#D9767C", marginRight: "4.32px" }}
-          />
-        </StyledBtn>
-        <p style={{ fontSize: "12px", color: "#D9767C" }}>{gifs.heart}</p>
-      </MapList>
-    </GIFBox>
-  ));
+  const gifList2 = gifs2.map((gifs, index) => {
+    let url = `/boards/:${gifs.id}`;
+    return (
+      <GIFBox key={index}>
+        <Link to={url}>
+          <StyledBtn>
+            <img
+              src={gifs.imageUrl}
+              alt="짤 썸네일"
+              style={{ width: 98, height: 86 }}
+            />
+          </StyledBtn>
+        </Link>
+        <MapList>
+          <StyledBtn>
+            <FontAwesomeIcon
+              icon={faHeart}
+              style={{
+                fontSize: "80%",
+                color: `${mainOrange}`,
+                marginRight: "4.32px",
+              }}
+            />
+          </StyledBtn>
+          <p style={{ fontSize: "12px", color: `${mainOrange}` }}>
+            {gifs.heart}
+          </p>
+        </MapList>
+      </GIFBox>
+    );
+  });
 
   // 전체 짤 조회, response data 가공 필요
   const allImage = () => {
+    let url = `/boards/image?page=${page}&sort=${sort1}&sort=${sort2}&q=`;
+    console.log(url);
     axios
-      .get(
-        preURL.preURL +
-          `/boards/image?page=${page}&size=10&sort=${sort}&q=검색어`
-      )
+      .get(preURL.preURL + url)
       .then((res) => {
         console.log("❕전체 짤 조회❕ ", res.data);
-        setGIFs1(res.data.slice(0, 4));
-        setGIFs2(res.data.slice(4));
+        setTotalPage(res.data.totalPageCount);
+        let gif1 = res.data.imageBoardsResponses.slice(0, 4);
+        let gif2 = res.data.imageBoardsResponses.slice(4);
+        setGIFs1(gif1);
+        setGIFs2(gif2);
       })
       .catch((err) => {
         console.log("⚠️ 전체 짤 조회 ⚠️ ", err);
-      });
-  };
-
-  // 상세 짤 조회, response data 가공 필요
-  const image = () => {
-    axios
-      .get(preURL.preURL + `/boards/image/${1}`) // imageID 넣기
-      .then((res) => {
-        console.log("❕상세 짤 조회❕ ", res.data);
-      })
-      .catch((err) => {
-        console.log("⚠️ 상세 짤 조회 ⚠️ ", err);
       });
   };
 
@@ -196,21 +222,7 @@ const GIFBoard = () => {
       });
   };
 
-  const pageNum = pages.map((page) => (
-    <StyledBtn
-      style={{ fontSize: "20px", padding: "10.5px" }}
-      onClick={() => {
-        setPage(page);
-        allImage();
-        console.log(`페이지 변경 - ${page}`);
-      }}
-    >
-      {page}
-    </StyledBtn>
-  ));
-
   // Modal
-
   const handleChange = ({ target: { value } }) => setTitle(value);
 
   const handleDelete = (i) => {
@@ -364,16 +376,16 @@ const GIFBoard = () => {
           <BestGIF src={Best_GIF} />
           <ListWrapper>{bestGIFList}</ListWrapper>
         </BestGIFWrapper>
-        <div>
+        <StyledDivColumn style={{ alignItems: "center" }}>
           <ListWrapper>{gifList1}</ListWrapper>
           <Line />
           <ListWrapper>{gifList2}</ListWrapper>
           <Line />
-        </div>
-        <SortBox>
+        </StyledDivColumn>
+        <Sort2Box>
           <StyledBtn
             style={{ fontSize: "10px", color: "#9E8FA8", marginRight: 4 }}
-            onClick={() => setSort("id, DESC")}
+            onClick={() => setSort2("id,DESC")}
           >
             최신순
           </StyledBtn>
@@ -382,54 +394,17 @@ const GIFBoard = () => {
           </p>
           <StyledBtn
             style={{ fontSize: "10px", color: "#9E8FA8" }}
-            onClick={() => setSort("likeCount, DESC")}
+            onClick={() => setSort2("likeCount,DESC")}
           >
             좋아요순
           </StyledBtn>
-        </SortBox>
-        <PageNums>
-          {pages != [1, 2, 3, 4, 5] ? (
-            <FontAwesomeIcon
-              icon={faCaretLeft}
-              style={{
-                fontSize: "20px",
-                color: "#9C9C9C",
-                marginLeft: "10.5px",
-              }}
-              onClick={() => {
-                console.log(`이전 페이지 리스트 - ${pages}`);
-                let newPages = [];
-                for (let i = 1; i <= 10; i++) {
-                  newPages.push(i);
-                }
-                setPages(newPages);
-              }}
-            />
-          ) : (
-            <></>
-          )}
-
-          <PageNum>{pageNum}</PageNum>
-          <StyledBtn>
-            {/* 다음 페이지가 없을 때의 경우 처리 필요 */}
-            <FontAwesomeIcon
-              icon={faCaretRight}
-              style={{
-                fontSize: "20px",
-                color: "#9C9C9C",
-                marginLeft: "10.5px",
-              }}
-              onClick={() => {
-                console.log(`다음 페이지 리스트 - ${pages}`);
-                let newPages = [];
-                for (let i = 1; i <= 10; i++) {
-                  newPages.push(i);
-                }
-                setPages(newPages);
-              }}
-            />
-          </StyledBtn>
-        </PageNums>
+        </Sort2Box>
+        <Pagination
+          pages={pages}
+          setPages={setPages}
+          setPage={setPage}
+          totalPageCount={totalPage}
+        />
       </Wrapper>
     </div>
   );
@@ -452,12 +427,14 @@ const BestGIFWrapper = styled.div`
   align-items: center;
   padding-top: 44px;
   padding-bottom: 27px;
-  border-bottom: 1px solid #9c9c9c;
+  border-bottom: 3px dashed #000000;
 `;
 
 const BestGIF = styled.img`
-  width: 88px;
-  height: 90px;
+  width: 242.2px;
+  height: 239.4px;
+  position: relative;
+  top: -27px;
 `;
 
 const ListWrapper = styled.div`
@@ -474,31 +451,18 @@ const MapList = styled.div`
 `;
 
 const GIFBox = styled.div`
-  margin: 31px 47px 12px 47px;
+  margin: 31px 40px 12px 40px;
 `;
 
 const Line = styled.div`
-  border-bottom: 1px solid #9c9c9c;
+  width: 843px;
+  border-bottom: 3px dashed #000000;
 `;
 
-const SortBox = styled.div`
+const Sort2Box = styled.div`
   width: 950px;
   display: flex;
   justify-content: flex-end;
-`;
-
-const PageNums = styled.div`
-  width: 221px;
-  height: 17px;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-`;
-
-const PageNum = styled.div`
-  display: flex;
-  flex-direction: row;
 `;
 
 // 새 글 쓰기 모달
--- src/Pages/Board/GIFBoardDetail.js
+++ src/Pages/Board/GIFBoardDetail.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import styled from "styled-components";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -11,37 +11,58 @@ import Temp_gif from "../../Assets/Temp_gif.png";
 import StyledBtn from "../../Style/StyledBtn";
 import axios from "axios";
 import preURL from "../../preURL/preURL";
+import { light } from "../../Style/Color";
+import { StyledDivRow } from "../../Style/StyledDiv";
 
 const GIFBoardDetail = ({ match }) => {
-  const { Id } = useParams();
+  const Id = useParams().id.substring(1);
   console.log("Id >>>", Id);
+
+  const mainOrange = light.colors.mainColor;
 
   const [detailInfo, setDetailInfo] = useState({
     id: 0,
-    title: "제목",
-    imageURL: Temp_gif,
+    imageBoardTitle: "제목",
+    imageUrl: Temp_gif,
     createdTime: 1,
     LocalDateTime: 1,
     viewCount: 0,
     likeCount: 0,
-    wrtieNickname: "me",
+    writerId: 0,
+    writerNickname: "me",
     isThisUserWriter: false,
-    isThisBoardLikedByUser: false,
-    tags: ["1", "2", "3"],
+    hashtags: ["1", "2", "3"],
   });
+  const [likeCount, setLikeCount] = useState(detailInfo.likeCount);
 
   useEffect(() => {
     console.log(
       "==========================[GIFBoardDetail]=========================="
     );
+    imgDetail();
   }, []);
+
+  // 상세 짤 조회
+  const imgDetail = () => {
+    console.log(`/boards/image/${Id}`);
+    axios
+      .get(preURL.preURL + `/boards/image/${Id}`)
+      .then((res) => {
+        console.error("❕상세 짤 조회❕ ", res.data);
+        setDetailInfo(res.data);
+      })
+      .catch((err) => {
+        console.error("⚠️ 상세 짤 조회 ⚠️ ", err);
+      });
+  };
 
   // 등록된 건지 취소된건지 보이는 팝업이나 시각적 요소 필요
   const like = () => {
     axios
-      .post(preURL.preURL + `/boards/image/${Id}`)
+      .post(preURL.preURL + `/boards/image/${Id}/likes`)
       .then((res) => {
         console.error("❕짤 좋아요 등록/취소❕ ", res.data);
+        setLikeCount(res.data);
       })
       .catch((err) => {
         console.error("⚠️ 짤 좋아요 등록/취소 ⚠️ ", err);
@@ -53,6 +74,9 @@ const GIFBoardDetail = ({ match }) => {
       .post(preURL.preURL + `/boards/image/${Id}/reports`)
       .then((res) => {
         console.log("❕짤 신고❕ ", res.data);
+        alert(
+          "해당 짤이 신고되었습니다. 신고가 누적되면 해당 짤은 삭제됩니다."
+        );
       })
       .catch((err) => {
         console.error("⚠️ 짤 신고 ⚠️ ", err);
@@ -60,14 +84,19 @@ const GIFBoardDetail = ({ match }) => {
   };
 
   const deleteImg = () => {
-    axios
-      .delete(preURL.preURL + `/boards/image/${Id}`)
-      .then((res) => {
-        console.log("❕짤 삭제❕ ", res.data);
-      })
-      .catch((err) => {
-        console.error("⚠️ 짤 삭제 ⚠️ ", err);
-      });
+    {
+      detailInfo.isThisUserWriter
+        ? axios
+            .delete(preURL.preURL + `/boards/image/${Id}`)
+            .then((res) => {
+              console.log("❕짤 삭제❕ ", res);
+              alert("짤이 삭제되었습니다.");
+            })
+            .catch((err) => {
+              console.error("⚠️ 짤 삭제 ⚠️ ", err);
+            })
+        : alert("이 짤을 등록한 작성자가 아닙니다. 삭제할 수 없습니다.");
+    }
   };
 
   return (
@@ -94,45 +123,37 @@ const GIFBoardDetail = ({ match }) => {
                   marginRight: 15,
                 }}
               >
-                {detailInfo.title}
-              </p>
-              <FontAwesomeIcon
-                icon={faHeart}
-                style={{
-                  fontSize: "150%",
-                  color: "#D9767C",
-                  marginRight: "4.32px",
-                }}
-              />
-              <p style={{ color: "#D9767C", fontSize: 36, margin: 0 }}>
-                {detailInfo.likeCount}
+                {detailInfo.imageBoardTitle}
               </p>
             </div>
-            <StyledBtn
-              style={{
-                backgroundColor: "#505050",
-                color: "white",
-                fontSize: 16,
-              }}
-              onClick={() => deleteImg()}
-            >
-              삭제
-            </StyledBtn>
           </InfoDetail>
           <InfoDetail>
-            <Detail style={{ color: "#B1A5B9" }}>
-              {detailInfo.wrtieNickname}
+            <Detail style={{ color: `${mainOrange}` }}>
+              {detailInfo.writerNickname}
             </Detail>
             <Detail>|</Detail>
             <Detail>{detailInfo.createdTime}</Detail>
             <Detail>{detailInfo.LocalDateTime}</Detail>
             <Detail>|</Detail>
             <Detail>조회 {detailInfo.viewCount}</Detail>
+            <Detail>|</Detail>
+            <StyledDivRow>
+              <FontAwesomeIcon
+                icon={faHeart}
+                style={{
+                  color: `${mainOrange}`,
+                  marginRight: "4.32px",
+                }}
+              />
+              <p style={{ color: `${mainOrange}`, margin: 0 }}>
+                {detailInfo.likeCount}
+              </p>
+            </StyledDivRow>
           </InfoDetail>
         </Info>
         <BottomWrapper>
           <img
-            src={detailInfo.imageURL}
+            src={detailInfo.imageUrl}
             style={{ width: 292.3, height: 266.88, borderRadius: 14 }}
           />
           <div
@@ -143,18 +164,51 @@ const GIFBoardDetail = ({ match }) => {
               marginBottom: 30,
             }}
           >
-            <Tag>#{detailInfo.tags[0]}</Tag>
-            <Tag>#{detailInfo.tags[1]}</Tag>
-            <Tag>#{detailInfo.tags[2]}</Tag>
+            <Tag>#{detailInfo.hashtags[0]}</Tag>
+            {detailInfo.hashtags.length > 1 ? (
+              <Tag>#{detailInfo.hashtags[1]}</Tag>
+            ) : (
+              <></>
+            )}
+            {detailInfo.hashtags.length > 2 ? (
+              <Tag>#{detailInfo.hashtags[2]}</Tag>
+            ) : (
+              <></>
+            )}
           </div>
           <div>
-            <StyledBtnWhite onClick={() => like()}>
-              <BtnContentPurple>좋아요</BtnContentPurple>
-            </StyledBtnWhite>
-            <StyledBtnRed onClick={() => report()}>
-              <BtnContentWhite>신고</BtnContentWhite>
-            </StyledBtnRed>
+            <StyledBtnWhite onClick={() => like()}>좋아요</StyledBtnWhite>
+            <StyledBtnRed onClick={() => report()}>신고</StyledBtnRed>
           </div>
+          {detailInfo.isThisUserWriter ? (
+            <Link to="/boards">
+              <StyledBtn
+                style={{
+                  backgroundColor: "none",
+                  color: "white",
+                  fontSize: 16,
+                  position: "relative",
+                  right: -320,
+                }}
+                onClick={() => deleteImg()}
+              >
+                삭제
+              </StyledBtn>
+            </Link>
+          ) : (
+            <StyledBtn
+              style={{
+                backgroundColor: "none",
+                color: "white",
+                fontSize: 16,
+                position: "relative",
+                right: -320,
+              }}
+              onClick={() => deleteImg()}
+            >
+              삭제
+            </StyledBtn>
+          )}
         </BottomWrapper>
       </Wrapper>
     </>
@@ -165,12 +219,17 @@ export default GIFBoardDetail;
 
 const Wrapper = styled.div`
   position: absolute;
-  top: 50%;
+  top: 55%;
   left: 50%;
   transform: translate(-50%, -50%);
-  width: 70%;
-  background-color: #505050;
+  background-color: #000000;
   padding: 3%;
+  width: 55%;
+  height: 80%;
+
+  box-sizing: border-box;
+  border: 6px dashed #ffffff;
+  border-radius: 50px;
 `;
 
 const Info = styled.div`
@@ -184,9 +243,9 @@ const InfoDetail = styled.div`
 `;
 
 const Detail = styled.p`
-  color: white;
-  font-size: 16;
-  margin-right: 5px;
+  color: #9c9c9c;
+  font-size: 14;
+  margin-right: 10px;
 `;
 
 const BottomWrapper = styled.div`
@@ -203,30 +262,30 @@ const Tag = styled.p`
   margin: 0px 5px 0px 5px;
 `;
 
-const StyledBtnRed = styled(StyledBtn)`
-  background-color: #d9767c;
-  border-radius: 29px;
-  margin: 10px;
-  padding-left: 25px;
-  padding-right: 25px;
-`;
-
 const StyledBtnWhite = styled(StyledBtn)`
+  width: 146px;
+  height: 58px;
   background-color: #ffffff;
   border-radius: 29px;
   margin: 10px;
+  margin-right: 50px;
   padding-left: 25px;
   padding-right: 25px;
+  background: #ffffff;
+  border: 5px dashed #000000;
+  border-radius: 29px;
 `;
 
-const BtnContentWhite = styled.p`
-  color: white;
-  font-size: 16;
-  font-weight: bold;
-`;
-
-const BtnContentPurple = styled.p`
-  color: #532a6b;
-  font-size: 16;
-  font-weight: bold;
+const StyledBtnRed = styled(StyledBtn)`
+  width: 146px;
+  height: 58px;
+  background-color: #d9767c;
+  border-radius: 29px;
+  margin: 10px;
+  margin-left: 50px;
+  padding-left: 25px;
+  padding-right: 25px;
+  background: #ffffff;
+  border: 3px dashed #000000;
+  border-radius: 29px;
 `;
