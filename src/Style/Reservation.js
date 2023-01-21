3088f134b366c316131fb26e735382c733432dca
SeryeongK <99seryoung50@gmail.com>
2023-01-21 22:55:24 +0900

    design: 달리기 대기 페이지 디자인 수정

--- src/Style/Reservation.js
+++ src/Style/Reservation.js
@@ -1,8 +1,18 @@
 import styled from "styled-components";
-import StyledBtn from "./StyledBtn";
 
-export const Wrapper = styled(StyledBtn)`
-  display: flex;
-  justify-content: center;
+// Assets
+import Reservation_bg from "../Assets/Reservation_bg.png";
+import { StyledDivColumn } from "./StyledDiv";
+
+export const Wrapper = styled(StyledDivColumn)`
+  height: 100%;
+  width: 100vw;
+  background-image: url(${Reservation_bg});
+  background-repeat: no-repeat;
+  background-position: top;
+  background-size: cover;
+  min-width: 100%;
+  min-height: 2675px;
+  padding-top: 9%;
   align-items: center;
 `;
