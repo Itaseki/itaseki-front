1e9f2e69a2fa02fc69f39ec805aa1f10c1a71aa0
hyowonii <hyowon9964@gmail.com>
2023-01-22 21:23:34 +0100

    Fix: 삭제된 코드 복구

--- src/Style/Running.js
+++ src/Style/Running.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import {light} from "./Color";
 
 export const Wrapper = styled.div`
   margin: 20px;
@@ -40,13 +41,13 @@ export const Chatting = styled.div`
 `;
 
 export const TopBar = styled.div`
-  background-color: #f9dca7;
+  background-color: ${light.colors.lightOrange};
   height: 22px;
 `;
 
 export const Inform = styled.div`
   margin: 5px;
-  background-color: #e8e8e8;
+  background-color: ${light.colors.lightGray};
 `;
 
 export const InformHeader = styled.div`
@@ -58,13 +59,11 @@ export const InformHeader = styled.div`
 
 export const Information = styled.div`
   position: absolute;
-  display: ${(props) => (props.show ? "contents" : "none")};
-  background-color: #e8e8e8;
-  // 수정 필요(임시)
-  width: 27.65%;
-  visibility: ${(props) => (props.show ? "visible" : "hidden")};
-  // display: ${(props) => (props.show ? "contents" : "none")};  // 원래 의도
-  background-color: #e8e8e8;
+  background-color: ${light.colors.lightGray};
+  
+  // TODO
+  visibility: ${props => props.show? "visible" : "hidden"};
+  // display: ${props => props.show? "contents" : "none"};  // 원래 의도
   & > p {
     padding: 0 10px;
     font-weight: bold;
@@ -106,7 +105,7 @@ export const ImgBtn = styled.img`
 `;
 
 export const ChatInput = styled.input`
-  background-color: #f9dca7;
+  background-color: ${light.colors.lightOrange};
   border: none;
   border-radius: 71px;
   width: 70%;
