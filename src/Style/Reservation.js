b21e1aed70724dc1a2b96a793450fbb5a27c51aa
SeryeongK <99seryoung50@gmail.com>
2023-01-24 01:31:43 +0900

    fix: 배경 이미지 제거(로딩 시간 문제)

--- src/Style/Reservation.js
+++ src/Style/Reservation.js
@@ -7,10 +7,6 @@ import { StyledDivColumn } from "./StyledDiv";
 export const Wrapper = styled(StyledDivColumn)`
   height: 100%;
   width: 100vw;
-  background-image: url(${Reservation_bg});
-  background-repeat: no-repeat;
-  background-position: top;
-  background-size: cover;
   min-width: 100%;
   min-height: 2675px;
   padding-top: 9%;
